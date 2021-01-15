eef33a01837c4b5ac48f0af60a329cc53366273c
rikeshshrestha <rikeshshrestha@gmail.com>
2021-01-15 18:56:17 +0545

    another testing

--- src/db/connection.js
+++ src/db/connection.js
@@ -9,7 +9,10 @@ const pool = new Pool({
     max: 20,
     idleTimeoutMillis: 30000,
     connectionTimeoutMillis: 2000,
-    ssl: true,
+    ssl: {
+        require: true,
+        rejectUnauthorized: false,
+    },
 })
 
 pool.connect((err, client, release) => {
