ddc71c44055f0fa1d77ceb2b38edea62bd7d5ba2
rikeshshrestha <rikeshshrestha@gmail.com>
2021-03-09 22:06:49 +0545

    update: working in docker and microservice

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -5,10 +5,15 @@ import * as userController from '../controller/user'
 
 let router = Router();
 
-router.get('/', userController.findAll)
-router.get('/:id', userController.findById)
-router.post('/', userController.create)
-router.put('/:id', userController.updateById)
-router.delete('/:id', userController.deleteById)
+try{
+
+    router.get('/', userController.findAll)
+    router.get('/:id', userController.findById)
+    router.post('/', userController.create)
+    router.put('/:id', userController.updateById)
+    router.delete('/:id', userController.deleteById)
+}catch(e){
+console.log('eeee',e)
+}
 
 export default router;
