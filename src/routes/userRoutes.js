32c1fa31d8b4cb2d5b20336a7847e7fa0f837f87
rikeshshrestha <rikeshshrestha@gmail.com>
2021-05-12 14:22:27 +0545

    update:: work remaining finding ways to represent error message

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,18 +2,18 @@ import {
     Router
 } from 'express'
 import * as userController from '../controller/user'
+import {customLog} from "../helper/log";
 
 let router = Router();
 
 try{
-
     router.get('/', userController.findAll)
     router.get('/:id', userController.findById)
     router.post('/', userController.create)
     router.put('/:id', userController.updateById)
     router.delete('/:id', userController.deleteById)
 }catch(e){
-console.log('eeee',e)
+    customLog("error",e)
 }
 
 export default router;
